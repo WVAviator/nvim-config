1f5d4d8ee14984c3e02adb0fd5fc36ec7cb4deb1
Wvaviator <wvaviator@gmail.com>
2023-07-03 11:34:47 -0500

    add nvim-tree

---
+++ after/plugin/nvim-tree.lua
@@ -0,0 +1,11 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.opt.termguicolors = true
+
+require("nvim-tree").setup()
+
+vim.keymap.set("n", "<leader>tt", vim.cmd.NvimTreeToggle)
+vim.keymap.set("n", "<leader>tc", vim.cmd.NvimTreeClose)
+vim.keymap.set("n", "<leader>tr", vim.cmd.NvimTreeRefresh)
+vim.keymap.set("n", "<leader>tf", vim.cmd.NvimTreeFindFile)
